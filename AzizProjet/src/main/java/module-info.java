47e0ce659bd334b0417eb9d71fe705ae254472ca
azizferjaoui <100564717+azizferjaoui@users.noreply.github.com>
2024-04-23 00:42:45 +0100

    rarting

--- AzizProjet/src/main/java/module-info.java
+++ AzizProjet/src/main/java/module-info.java
@@ -2,8 +2,9 @@ module com.projet.azizprojet {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires java.sql;
+    requires org.controlsfx.controls;
 
-	opens com.projet.azizprojet.Controllers to javafx.fxml;
+    opens com.projet.azizprojet.Controllers to javafx.fxml;
 
 	opens com.projet.azizprojet to javafx.fxml;
 	exports com.projet.azizprojet;
