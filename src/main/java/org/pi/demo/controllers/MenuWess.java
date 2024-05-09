0a72d805d080336514e542a56f0b9cd9a76c2d45
FLAWTO <wassim.frigui@esprit.tn>
2024-05-09 03:36:18 +0100

    Menu

--- src/main/java/org/pi/demo/controllers/MenuWess.java
+++ src/main/java/org/pi/demo/controllers/MenuWess.java
@@ -13,7 +13,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.Objects;
 
-public class MenuController {
+public class MenuWess {
     @FXML
     private TextField aff;
     @FXML
